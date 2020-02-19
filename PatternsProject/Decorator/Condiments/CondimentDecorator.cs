5a6e037b2f0b583e79f19e19b3c3e39d61099032
Alexandru Rosca <alexandru.rosca@amdaris.com>
2020-02-19 13:40:46 +0200

    Finished Decorator

--- PatternsProject/Decorator/Condiments/CondimentDecorator.cs
+++ PatternsProject/Decorator/Condiments/CondimentDecorator.cs
@@ -5,8 +5,6 @@ using System.Text;
 namespace PatternsProject.Decorator.Condiments
 {
     abstract class CondimentDecorator : Beverage
-    {
-        public abstract string GetDescription();
-
+    { 
     }
 }
